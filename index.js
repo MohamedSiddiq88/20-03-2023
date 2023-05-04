eec857a0ee69046c9bbc7ec01cfb8a61adcf13b7
MohamedSiddiq88 <124970161+MohamedSiddiq88@users.noreply.github.com>
2023-05-04 23:13:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-let disp=document.getElementById("disp");
-let count=10;
-function int(){
-    disp.innerText=count;
-    if(count==0){
-    clearInterval(inter);
-    disp.innerText="Happy Independence Day";
-    }
-    count--;
-}
-let inter=setInterval(int,1000);
+
+// get the countdown element
+let countdown=document.getElementById("countdown");
+let countdownContainer=document.getElementById("countdown-container");
+let img=document.getElementById("img");
+
+
+
+//insert value
+setTimeout(function() {
+    countdown.innerText="10";
+    setTimeout(function() {
+        countdown.innerText="9";
+      setTimeout(function() {
+        countdown.innerText="8";
+        setTimeout(function() {
+            countdown.innerText="7";
+          setTimeout(function() {
+            countdown.innerText="6";
+            setTimeout(function() {
+                countdown.innerText="5";
+              setTimeout(function() {
+                countdown.innerText="4";
+                setTimeout(function() {
+                    countdown.innerText="3";
+                  setTimeout(function() {
+                    countdown.innerText="2";
+                    setTimeout(function() {
+                        countdown.innerText="1";
+                      setTimeout(function() {
+                        countdown.innerText="1";
+                        setTimeout(function() {
+                            countdownContainer.style="display:none";
+                            img.style="display:flex";
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
